24b1f64d5bdbec72a3526fd4460bb2d2e4597e63
alessioharo <alessio.haro@icloud.com>
2025-07-11 10:21:26 -0500

    Updated colors

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <a href="index.php">
-    <header><img src="./images/Herb Logo Placeholder.png" alt="Herb" class="mainlogo"></header>
+    <header><img src="./images/Herb Logo.png" alt="Herb" class="mainlogo"></header>
 </a>

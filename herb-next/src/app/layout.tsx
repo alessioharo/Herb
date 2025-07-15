d83193b936d83ebf61d4848efe167e819bc333f3
alessioharo <alessio.haro@icloud.com>
2025-07-15 18:48:06 -0500

    Updated web icon

--- herb-next/src/app/layout.tsx
+++ herb-next/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/H Logo.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         <main>

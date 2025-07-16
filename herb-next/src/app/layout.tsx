32d2c03a332df2063d88f5efe191a5c44dd260a3
alessioharo <alessio.haro@icloud.com>
2025-07-15 20:57:39 -0500

    Commented out unused code

--- herb-next/src/app/layout.tsx
+++ herb-next/src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+/*const inter = Inter({ subsets: ['latin'] });*/
 
 export const metadata = {
   title: 'Herb',

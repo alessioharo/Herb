a23424d275f784eb986c5017cfc0bd0d284d8750
alessioharo <alessio.haro@icloud.com>
2025-07-15 22:34:44 -0500

    Updated ignores

--- herb-next/eslint.config.mjs
+++ herb-next/eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignorePatterns: ["src/generated/prisma/"]
+    ignores: ["src/generated/prisma/"]
   },
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript'],

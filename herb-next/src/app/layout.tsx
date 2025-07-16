542b199a58edc9f7865609fd5526f33161d18f27
alessioharo <alessio.haro@icloud.com>
2025-07-16 14:36:53 -0500

    Cleaned up comments in globals

--- herb-next/src/app/layout.tsx
+++ herb-next/src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Herb" />
       </head>
       <body>
         <main>

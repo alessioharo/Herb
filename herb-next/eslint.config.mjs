86aa879c5056038b29bd5f3a7ba1e616ccc74a1b
alessioharo <alessio.haro@icloud.com>
2025-07-15 22:05:43 -0500

    Attempted to run npx prisma generate to resolve issues

--- herb-next/eslint.config.mjs
+++ herb-next/eslint.config.mjs
@@ -15,10 +15,10 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-require-imports': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
+      /*'@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-wrapper-object-types': 'warn',
       '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
-      
+      '@typescript-eslint/no-unused-expressions': 'warn',*/
     }
   }),
 ];

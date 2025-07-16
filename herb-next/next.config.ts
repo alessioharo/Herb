d87ad1bd1686e4b2f1aa3ad088a01b2369a2d46a
alessioharo <alessio.haro@icloud.com>
2025-07-15 22:13:04 -0500

    Made sure tsconfig.json includes generated prisma directory

--- herb-next/next.config.ts
+++ herb-next/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;

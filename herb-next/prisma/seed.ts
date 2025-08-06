01e232ce2b5657a0203113713433d8524e4974e6
Alessio Haro <alessio.haro@icloud.com>
2025-08-06 14:52:08 -0500

    Regenerated prisma client

--- herb-next/prisma/seed.ts
+++ herb-next/prisma/seed.ts
@@ -1,6 +1,6 @@
 
 import comicsData from './comics.json';
-import { PrismaClient } from '../src/generated/prisma';
+import { PrismaClient } from '@/generated/prisma';
 const prisma = new PrismaClient();
 
 type ComicSeed = {
